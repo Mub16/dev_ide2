46b2d53c0c4f3121643cceafe5a2a5410e04b14f
Ivan16 <47645729+Mub16@users.noreply.github.com>
2021-09-22 15:44:56 +0400

    Ivan
    Удалён файл engen.php

--- src/engen.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $_SERVER['QUERY_STRING'];
-    $routes = ['C:/Code/dev_ide1/src' => 'index.php'];
-    echo 404;
-    foreach ($routes as $key => $val) {
-    if ($key == $_SERVER['QUERY_STRING']) require $val;
-    };
-?>
